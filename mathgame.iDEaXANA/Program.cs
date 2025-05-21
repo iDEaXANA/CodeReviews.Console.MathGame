9401139d772898a5756ff62c34f736c6e5b3025b
Bilal Mohamed-Benkada <bmbenkada@gmail.com>
2025-05-21 19:04:14 +0100

    Fully functioning v1 of the mathgame

--- mathgame.iDEaXANA/Program.cs
+++ mathgame.iDEaXANA/Program.cs
@@ -5,124 +5,125 @@
 //      a. each result 
 //      b. when dividing results in non integer value.
 
-List<string> saveStore = new List<string>();
-    Console.WriteLine("New Game");
-    Console.WriteLine("Saved Games"); 
+static string GetUserInputs()
+{
+    Console.WriteLine("Please pick your operators by pressing the corresponding key on your keyboard");
+    Console.WriteLine("+");
+    Console.WriteLine("-");
+    Console.WriteLine("*");
+    Console.WriteLine("/");
 
-    while (true)
+    string operatorInput = Console.ReadLine();
+    return operatorInput;
+}
+
+static (int firstNumber, int secondNumber, int dividend) GetRandomNumbers()
+{
+    Random rnd = new Random();
+    int firstNumber = rnd.Next(1, 10);
+    int secondNumber = rnd.Next(1, 10);
+    int dividend = rnd.Next(1, 101);
+
+    return (firstNumber, secondNumber, dividend);
+}
+
+static string GetDisplayResult(int firstNumber, string operatorInput, int secondNumber, int dividend, int result)
+{
+    if (operatorInput == "/")
     {
-    Console.WriteLine("Press the 'N' key to start a new game or the 'S' key to view saved games");
-
-    string? userInput = Console.ReadLine();
-
-    
-
-    static (int firstNumber, int secondNumber) GetUserInputs()
-    {
-        Console.WriteLine("Pick your first number. It can be any number, provided both numbers result in an integer.");
-        string? firstNumberInput = Console.ReadLine();
-        if (firstNumberInput == null) throw new InvalidOperationException("stop trolling");
-        int firstNumber = int.Parse(firstNumberInput);
-        Console.WriteLine("Don't worry... I'll let you know if the end result is an integer!");
-        Console.WriteLine("Now pick your second number. If dividing, this number must be between 0 and 100!");
-        string? secondNumberInput = Console.ReadLine();
-        if (secondNumberInput == null) throw new InvalidOperationException("Really... Again?");
-        int secondNumber = int.Parse(secondNumberInput);
-        Console.WriteLine("Now, please pick your operators by pressing the corresponding key on your keyboard");
-        Console.WriteLine("+");
-        Console.WriteLine("-");
-        Console.WriteLine("*");
-        Console.WriteLine("/");
-
-        return (firstNumber, secondNumber);
+        return $"{dividend} {operatorInput} {secondNumber} = {result}";
     }
+    return $"{firstNumber} {operatorInput} {secondNumber} = {result}";
+}
 
+
+List<string> saveStore = new List<string>();
+Console.WriteLine("New Game");
+Console.WriteLine("Saved Games");
+
+while (true)
+{
+    Console.WriteLine("Press the 'N' key to start a new game or the 'S' key to view saved games");
+    string? userInput = Console.ReadLine();
+    int result;
     if (userInput == "n" || userInput == "N")
     {
-        (int firstNumber, int secondNumber) = GetUserInputs();
-        var operatorInput = Console.ReadLine();
-
+        string operatorInput = GetUserInputs();
+        (int firstNumber, int secondNumber, int dividend) = GetRandomNumbers();
         if (operatorInput == "+")
         {
             // Do operation
-            var result = firstNumber + secondNumber;
-            // print operation
-            var displayResult = $"{firstNumber} {operatorInput} {secondNumber} = {result}";
-            Console.WriteLine(displayResult);
-            // save operation to a new list
-            saveStore.Add(displayResult);
+            result = firstNumber + secondNumber;
         }
         else if (operatorInput == "-")
         {
-            var result = firstNumber - secondNumber;
-            var displayResult = $"{firstNumber} {operatorInput} {secondNumber} = {result}";
+            result = firstNumber - secondNumber;
 
-            Console.WriteLine(displayResult);
-            saveStore.Add(displayResult);
         }
         else if (operatorInput == "*")
         {
-            var result = firstNumber * secondNumber;
-            var displayResult = $"{firstNumber} {operatorInput} {secondNumber} = {result}";
-
-            Console.WriteLine(displayResult);
-            saveStore.Add(displayResult);
+            result = firstNumber * secondNumber;
         }
         else if (operatorInput == "/")
         {
-            if (secondNumber <= 0 || secondNumber >= 101)
+            while (dividend % secondNumber != 0)
             {
-                Console.WriteLine("The Dividend must be between 0 and 100, including 100");
-                GetUserInputs();
+                (firstNumber, secondNumber, dividend) = GetRandomNumbers();
             }
-            var result = firstNumber / secondNumber;
-            if (result % 1 != 0)
-            {
-                Console.WriteLine("The result is not an integer. Restarting game...");
-                GetUserInputs();
-            }
-
-            var displayResult = $"{firstNumber} {operatorInput} {secondNumber} = {result}";
-
-            Console.WriteLine(displayResult);
-            saveStore.Add(displayResult);
+            result = dividend / secondNumber;
         }
+        else
+        {
+            Console.WriteLine("Invalid Operator. Please choose from the keys displayed on the screen.");
+            continue;
+        }
+        var displayResult = GetDisplayResult(firstNumber, operatorInput, secondNumber, dividend, result);
+        Console.WriteLine(displayResult);
+        saveStore.Add(displayResult);
     }
     else if (userInput == "s" || userInput == "S")
     {
+        if (saveStore.Count == 0)
+        {
+            Console.WriteLine("A game must be played first");
+            continue;
+        }
         int counter = 1;
         foreach (string entry in saveStore)
         {
-            
+
             Console.WriteLine($"Game No.{counter}: \"{entry}\"");
             counter++;
         }
-
     }
     else
     {
         Console.WriteLine("Please pick a valid entry key");
     }
 }
+// Current problems
+
+
 
 /* Random game To Do
  * Replace user input values with random values
- * For added difficulty, add a digit
- * Pick two random numbers
- * To follow division integer rule, ??
- * Save results in a new list
- * Display results based on gametype
-
- *
- */ 
-
-
-/*CHALLENGES
- *
- * Random questions game
- * Timer 
- * Azure AI input to ask questions
- * Get a code review.
+ * COMMIT
+ * For added difficulty, add a digit to dividend only.
+ * Add a pre check for calculation
+ * Allow user input and make sure the values match.
+ * Add Easy mode and Hard mode from menu for different pathways.
+ * COMMIT
  * 
- * 
+
+ *Add a timer:
+ *When game starts
+ *Ends when value matches correctly.
+ *COMMIT
+ *
+ *Add Random Operations
+ *COMMIT
+ 
+ *AI Challenge
+ *COMMIT
  */
+
